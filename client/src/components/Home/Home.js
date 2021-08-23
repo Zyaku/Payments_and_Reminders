c0d83b7dfc6e4a8aa5f2d95a0a21f71b44ecd232
Zyaku <huy.ngpriv@gmail.com>
2021-08-23 21:25:33 +0200

    terrible svg logo. Only use as placeholder. This is not good

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,16 +1,19 @@
 import react,{Component} from "react";
 import './Home.css';
+import {ReactComponent as CoinClock} from '../../assets/CoinClock.svg';
 class Home extends Component{
     render(){
         return(
             <div className="Home"> 
                 <div>   
                     <i className="Home-visuals"></i>
-                    <div className="placeholder"></div>
+                    <div className="placeholder"> 
+                        <div className="clock"><CoinClock/></div>
+                    </div> 
                 </div>
                 <div className="Description" >
                     <h3 className = "Desc-Header"> Payments and Reminders</h3>
-                    <p className="Desc-Body"> This is a project I made for learning Javascript, JSX and CSS. In this Project
+                    <p className="Desc-Body"> This is a project I made for learning Javascript and CSS. In this Project
                     I will implement a CRUD RESTful API with a React based Front-end. This website will simulate users sign in, 
                     logging in with an existing account, request, pay and view their payments. If a payment is overdue, an Email 
                     is sent to the User once as a reminder. User and payment data will be saved in a JSON file.
